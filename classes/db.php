611a0fdd8b0ff4720b88f86bec3b95d54b95e9fe
Thomas <tarozidoggen@gmail.com>
2026-03-05 11:55:21 +0100

    feature added(database connection + dummy data + database setup)

---
+++ classes/db.php
@@ -0,0 +1,18 @@
+<?php
+
+function dbconnect() {
+    $host = 'localhost'; 
+    $dbname = 'linguar_db';
+    $username = 'root'; 
+    $password = '';
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        die("Database connection failed: " . $e->getMessage());
+    }
+}
+
+?>
